4f0b5cfc6e466678c6b22475cc494cae264d7969
bedri <bedri@localhost.localdomain>
2026-06-05 21:02:25 +0300

    build: make container build script incremental by default

--- build_in_container.sh
+++ build_in_container.sh
@@ -1,33 +1,56 @@
 #!/bin/bash
 set -e
 
+CLEAN=false
+RECONFIGURE=false
+
+for arg in "$@"; do
+    case $arg in
+        --clean|-c)
+            CLEAN=true
+            ;;
+        --reconfigure|-r)
+            RECONFIGURE=true
+            ;;
+    esac
+done
+
 echo "Starting build inside Ubuntu container..."
 
-# Compile dependencies
-echo "Compiling dependencies..."
-cd depends
-chmod +x config.sub config.guess
-make -j30 HOST=x86_64-pc-linux-gnu
-cd ..
+# Compile dependencies (only if prefix directory doesn't exist or reconfigure requested)
+if [ ! -d "depends/x86_64-pc-linux-gnu" ] || $RECONFIGURE; then
+    echo "Compiling dependencies..."
+    cd depends
+    chmod +x config.sub config.guess
+    make -j30 HOST=x86_64-pc-linux-gnu
+    cd ..
+fi
+
+# Configure DSW (only if Makefile doesn't exist or reconfigure requested)
+if [ ! -f "Makefile" ] || $RECONFIGURE; then
+    echo "Configuring build system..."
+    chmod +x share/genbuild.sh autogen.sh
+    ./autogen.sh
+    ./configure --enable-glibc-back-compat \
+                --prefix=$(pwd)/depends/x86_64-pc-linux-gnu \
+                LDFLAGS="-static-libstdc++" \
+                --enable-cxx \
+                --enable-static \
+                --disable-shared \
+                --disable-debug \
+                --disable-bench \
+                --with-pic \
+                CPPFLAGS="-fPIC -O3 --param ggc-min-expand=1 --param ggc-min-heapsize=32768" \
+                CXXFLAGS="-fPIC -O3 --param ggc-min-expand=1 --param ggc-min-heapsize=32768"
+fi
+
+if $CLEAN; then
+    echo "Cleaning build..."
+    make clean
+fi
 
 # Compile DSW
 echo "Compiling DSW..."
-chmod +x share/genbuild.sh autogen.sh
-./autogen.sh
-./configure --enable-glibc-back-compat \
-            --prefix=$(pwd)/depends/x86_64-pc-linux-gnu \
-            LDFLAGS="-static-libstdc++" \
-            --enable-cxx \
-            --enable-static \
-            --disable-shared \
-            --disable-debug \
-            --disable-bench \
-            --with-pic \
-            CPPFLAGS="-fPIC -O3 --param ggc-min-expand=1 --param ggc-min-heapsize=32768" \
-            CXXFLAGS="-fPIC -O3 --param ggc-min-expand=1 --param ggc-min-heapsize=32768"
-
-make clean
 make -j24 HOST=x86_64-pc-linux-gnu
 
 echo "Build completed successfully!"
-
