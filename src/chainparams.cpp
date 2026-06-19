9a7a151fe2ce245d3d46b444c9f151b0ee76816e
bedri <bedri@localhost.localdomain>
2026-06-19 22:56:35 +0300

    Configure 8MB blocks and 10s target spacing / time slots

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -125,8 +125,8 @@ public:
         consensus.nStakeMinDepthV2 = 600;
         consensus.nTargetTimespan = 40 * 60;
         consensus.nTargetTimespanV2 = 30 * 60;
-        consensus.nTargetSpacing = 30;
-        consensus.nTimeSlotLength = 15;
+        consensus.nTargetSpacing = 10;
+        consensus.nTimeSlotLength = 10;
 
         // ADAM consensus parameters
         consensus.nAdamMinersCount = 11;
@@ -251,8 +251,8 @@ public:
         consensus.nStakeMinDepthV2 = 30;
         consensus.nTargetTimespan = 40 * 60;
         consensus.nTargetTimespanV2 = 30 * 60;
-        consensus.nTargetSpacing = 30;
-        consensus.nTimeSlotLength = 15;
+        consensus.nTargetSpacing = 10;
+        consensus.nTimeSlotLength = 10;
 
         // ADAM consensus parameters
         consensus.nAdamMinersCount = 11;
